dd15801b3a3f6c41a0fdb8e0188d1573c652c967
Pavel Sh <paulshipspb@gmail.com>
2023-04-17 21:21:13 +0300

    Вторую задачу перенесли в папку Reshenie_2

--- Program.cs
+++ Program.cs
@@ -14,19 +14,7 @@ else {
       Console.WriteLine($"Число {number} не является палиндромом");
     }
 
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-//A (3,6,8); B (2,1,-7), -> 15.84 A (7,-5, 0); B (1,-1,9) -> 11.53
 
-    Console.WriteLine("Введите координаты первой точки (x1, y1, z1):");
-    double x1 = double.Parse(Console.ReadLine());
-    double y1 = double.Parse(Console.ReadLine());
-    double z1 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Введите координаты второй точки (x2, y2, z2):");
-    double x2 = double.Parse(Console.ReadLine());
-    double y2 = double.Parse(Console.ReadLine());
-    double z2 = double.Parse(Console.ReadLine());
-    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-    Console.WriteLine($"Расстояние между точками ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно {distance}");
 
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125
